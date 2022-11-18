794bdf5a9a99e1334c524f8993d6569fc47be6cd
mireyachaparro <mireyacn99@gmail.com>
2022-11-18 20:56:28 +0100

    Add repository test

--- src/data/robot.repository.test.ts
+++ src/data/robot.repository.test.ts
@@ -1,62 +1,61 @@
 import mongoose from 'mongoose';
-import { dbConnect } from '../db.connect';
-import { RobotRepository } from './robot.repository';
+import { stringify } from 'querystring';
+import { dbConnect } from '../db.connect.js';
+import { RobotRepository } from './robot.repository.js';
 
 const mockData = [
     {
-        name: 'R2-D2',
-        img: 'RD2D.JPG',
+        name: 'prueba1',
+        img: '123.jpg',
         speed: 1,
         resistance: 2,
-        date: '1977',
+        date: '1999',
     },
     {
-        name: 'HK-47',
-        img: 'HK47.JPG',
+        name: 'prueba2',
+        img: '456.jpg',
         speed: 3,
         resistance: 4,
-        date: '2003',
+        date: '2000',
     },
 ];
-
 describe('Given RobotRepository', () => {
-    const repository = new RobotRepository();
-    let testIds: Array<string>;
-    beforeAll(async () => {
-        await dbConnect();
-        await repository.getModel().deleteMany();
-        await repository.getModel().insertMany(mockData);
-        const data = await repository.getModel().find();
-        testIds = [data[0].id, data[1].id];
-    });
+    describe('When we instantiate it', () => {
+        const repository = new RobotRepository();
+        let testIds: Array<string>;
 
-    test('Then getAll should return the name of the first register', async () => {
-        const result = await repository.getAll();
-        expect(result[0].name).toEqual(mockData[0].name);
-    });
+        beforeAll(async () => {
+            await dbConnect();
+            await repository.getModel().deleteMany();
+            await repository.getModel().insertMany(mockData);
+            const data = await repository.getModel().find();
+            testIds = [data[0].id, data[1].id];
+        });
+        afterAll(() => {
+            mongoose.disconnect();
+        });
 
-    test('Then post should return the new object', async () => {
-        const newRobot = {
-            name: 'johnny',
-        };
-        const result = await repository.post(newRobot);
-        expect(result.name).toEqual(newRobot.name);
-    });
+        test('Then getAll should have been called', async () => {
+            const result = await repository.getAll();
+            expect(result[0].name).toEqual(mockData[0].name);
+        });
 
-    test('Then patch should return the object with the updated property', async () => {
-        const updatedRobot = {
-            name: 'Tachikomas',
-        };
-        const result = await repository.patch(testIds[0], updatedRobot);
-        expect(result.name).toEqual(updatedRobot.name);
-    });
+        test('Then post should have been called', async () => {
+            const newRobot = {
+                name: 'prueba3',
+                img: '789.jpg',
+                speed: 5,
+                resistance: 6,
+                date: '2001',
+            };
+            const result = await repository.post(newRobot);
+            expect(result.name).toEqual(newRobot.name);
+        });
 
-    test('Then delete should return an empty object', async () => {
-        const result = await repository.delete(testIds[0]);
-        expect(result).toBeUndefined();
-    });
-
-    afterAll(() => {
-        mongoose.disconnect();
+        test('Then if id is bad formated delete should throw an error', async () => {
+            expect(async () => {
+                await repository.delete(2);
+            }).rejects.toThrowError(mongoose.Error.CastError);
+        });
     });
 });

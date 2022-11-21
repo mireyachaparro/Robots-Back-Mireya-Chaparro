814eadecc421d41a54f7f468aeec3ae5147a7b42
mireyachaparro <mireyacn99@gmail.com>
2022-11-21 20:23:30 +0100

    fix render

--- src/router/robots.router.ts
+++ src/router/robots.router.ts
@@ -1,13 +1,21 @@
 import { Router } from 'express';
 import { RobotController } from '../controller/robot.controller.js';
+// import { UserController } from '../controller/user.js';
 import { RobotRepository } from '../data/robot.repository.js';
+// import { UserRepository } from '../repositories/user.js';
 
 export const robotRouter = Router();
 
 const controller = new RobotController(new RobotRepository());
+// const userController = new UserController(new UserRepository());
 
 robotRouter.get('/', controller.getAll.bind(controller));
-robotRouter.get('/:id', controller.get.bind(controller));
+robotRouter.get(
+    '/:id',
+    // esto da error(Could not get response, Error: read ECONNRESET)
+    // userController.login.bind(controller),
+    controller.get.bind(controller)
+);
 robotRouter.post('/create', controller.post.bind(controller));
 robotRouter.patch('/update/:id', controller.patch.bind(controller));
 robotRouter.delete('/delete/:id', controller.delete.bind(controller));
